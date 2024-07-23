8f112882778872342a52c6520cf52e3383a3612d
Thiago Pereira <thiago@brandnexity.com>
2024-07-23 11:37:17 -0300

    Ajustes Pós Golive

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -340,7 +340,7 @@ function ProductCard({
                   ""
                 )
                 : (
-                  <h2 class="line-clamp-2 uppercase text-xs font-bold text-base-content">
+                  <h2 class="line-clamp-2 text-xs font-bold text-base-content">
                     {isVariantOf?.name || name}
                   </h2>
                 )}
