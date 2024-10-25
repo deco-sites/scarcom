e1111566094e0e066b4f2e2112128772bd60e3fa
Murilo Gabriel <murilofer@yahoo.com.br>
2024-10-25 18:46:49 -0300

    fix: atualizando componente de especificacoes da PDP

--- components/product/ProductAdvancedDetails/SpecificationsTab.tsx
+++ components/product/ProductAdvancedDetails/SpecificationsTab.tsx
@@ -3,25 +3,14 @@ import { ProductDetailsPage } from "apps/commerce/types.ts";
 function SpecificationsTab(
   { page, state }: { page: ProductDetailsPage; state: boolean },
 ) {
-  const { product: { additionalProperty, description } } = page;
-  const specifications = additionalProperty &&
-    additionalProperty.some((property) =>
-      property.propertyID?.toLocaleLowerCase() === "specification"
-    );
+  const { product: { description } } = page;
 
   return (
-    <div class={`${state ? "flex" : "hidden"}`}>
-      {specifications
-        ? (
-          <div class="not-italic font-semibold text-[14px] leading-[16px] text-[#848689]">
-          </div>
-        )
-        : (
-          <div
-            class="not-italic font-semibold text-[14px] leading-[16px] text-[#848689]"
-            dangerouslySetInnerHTML={{ __html: description ? description : "" }}
-          />
-        )}
+    <div class={` ${state ? "flex" : "hidden"}`}>
+      <div
+        class="not-italic font-semibold text-[14px] leading-[16px] text-[#848689]"
+        dangerouslySetInnerHTML={{ __html: description ? description : "" }}
+      />
     </div>
   );
 }
