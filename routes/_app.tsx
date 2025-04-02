f9bd201e73d9f7d47cfcec9c3074049c43781332
Lucas <lucas.silva@agencian1.com.br>
2025-04-02 17:34:25 -0300

    task11881 - fix: correçao do bug

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -35,7 +35,7 @@ export default defineApp(async (_req, ctx) => {
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.<wbr>com/gtm.js?id='+i+dl;f.<wbr>parentNode.insertBefore(j,f)https://www.googletagmanager.<wbr>com/gtm.js?id='+i+dl;f.<wbr>parentNode.insertBefore(j,f)https://www.googletagmanager.<wbr>com/gtm.js?id='+i+dl;f.<wbr>parentNode.insertBefore(j,f);
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f)
             })(window,document,'script','dataLayer','GTM-PD5C6K');
           `,
           }}
